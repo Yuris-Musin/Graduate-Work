e833bf0e7ff57c20ed1c002150ce26f7a261374a
Yuris <yuris.musin@mail.ru>
2025-01-29 18:41:19 +0500

    implemented the add to calendar feature

--- app/src/main/java/ru/musindev/graduate_work/domain/models/Segment.kt
+++ app/src/main/java/ru/musindev/graduate_work/domain/models/Segment.kt
@@ -3,5 +3,7 @@ package ru.musindev.graduate_work.domain.models
 data class Segment(
     val departure: String,
     val arrival: String,
-    val thread: ThreadInfo
+    val thread: ThreadInfo,
+    val departureTimeMillis: Long,
+    val arrivalTimeMillis: Long
 )
--- app/src/main/java/ru/musindev/graduate_work/views/adapters/ScheduleAdapter.kt
+++ app/src/main/java/ru/musindev/graduate_work/views/adapters/ScheduleAdapter.kt
@@ -1,8 +1,12 @@
 package ru.musindev.graduate_work.views.adapters
 
+import android.content.Context
+import android.content.Intent
+import android.provider.CalendarContract
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import ru.musindev.graduate_work.R
 import ru.musindev.graduate_work.databinding.ItemScheduleBinding
 import ru.musindev.graduate_work.domain.models.Segment
 
@@ -28,9 +32,25 @@ class ScheduleAdapter : RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder>
 
     class ScheduleViewHolder(private val binding: ItemScheduleBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(segment: Segment) {
-            binding.tvDeparture.text = "Отправление: " + segment.departure
-            binding.tvArrival.text = "Прибытие: " + segment.arrival
-            binding.tvRoute.text = "Маршрут: " + segment.thread.title
+            binding.tvDeparture.text = "Отправление: ${segment.departure}"
+            binding.tvArrival.text = "Прибытие: ${segment.arrival}"
+            binding.tvRoute.text = "Маршрут: ${segment.thread.title}"
+
+            binding.ivAddToCalendar.setOnClickListener {
+                binding.ivAddToCalendar.setImageResource(R.drawable.baseline_favorite_24)
+                addEventToCalendar(it.context, segment)
+            }
+        }
+
+        private fun addEventToCalendar(context: Context, segment: Segment) {
+            val intent = Intent(Intent.ACTION_INSERT).apply {
+                data = CalendarContract.Events.CONTENT_URI
+                putExtra(CalendarContract.Events.TITLE, "Поездка: ${segment.thread.title}")
+                putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, segment.departureTimeMillis)
+                putExtra(CalendarContract.EXTRA_EVENT_END_TIME, segment.arrivalTimeMillis)
+                putExtra(CalendarContract.Events.EVENT_LOCATION, segment.departure + " - " + segment.arrival)
+            }
+            context.startActivity(intent)
         }
     }
 }
